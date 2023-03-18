4f4cb3130cec112777249f579cfb36b3d01ba899
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2023-03-18 13:56:43 +0100

    commit

---
+++ src/main/java/org/sid/ebankservice/security/KeycloakConfig.java
@@ -0,0 +1,13 @@
+package org.sid.ebankservice.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+    @Bean
+    KeycloakSpringBootConfigResolver springBootConfigResolver(){
+        return  new KeycloakSpringBootConfigResolver();
+    }
+}
